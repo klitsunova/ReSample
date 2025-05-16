6aab0dcd140155146d69f5eb6b12afb178c2ccd4
klitsunova <kateryna.klitsunova@gmail.com>
2025-05-16 16:57:05 +0300

    Fix upload

--- modules/upload.py
+++ modules/upload.py
@@ -2,14 +2,6 @@ import streamlit as st
 import pandas as pd
 import time
 
-def upload_dataset():
-    data_option = st.radio("Choose dataset option:", ["Use Sample Data", "Upload New Data"], horizontal=True)
-    if data_option == "Upload New Data":
-        df = upload_new_dataset()
-    else:
-        df = upload_sample_dataset()
-    return df
-
 def upload_new_dataset():
     uploaded_file = st.file_uploader("📂 Upload your dataset (.csv or .xlsx)", type=["csv", "xlsx"])
     if uploaded_file is not None:
@@ -25,7 +17,9 @@ def upload_new_dataset():
         if df.select_dtypes(include=['object']).shape[1] > 0:
             st.error("❌ The uploaded dataset contains non-numeric values. Please upload a dataset with numerical features only.")
             df = None
-    return df
+        return df
+    else:
+        return None
 
 def upload_sample_dataset():
     sample_data_path = "sample_imbalanced_data_missing_data.csv"
@@ -34,4 +28,14 @@ def upload_sample_dataset():
         df = pd.read_csv(sample_data_path)
         st.success("✅ Using sample dataset.")
     return df
+
+
+def upload_dataset():
+    data_option = st.radio("Choose dataset option:", ["Use Sample Data", "Upload New Data"], horizontal=True)
+    if data_option == "Upload New Data":
+        df = upload_new_dataset()
+    else:
+        df = upload_sample_dataset()
+    return df
+
 
