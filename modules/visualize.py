4a09186157fb5a9a6d643069089e7af8f4f407b8
klitsunova <kateryna.klitsunova@gmail.com>
2025-05-16 17:55:17 +0300

    Change csv encoding to utf-8

--- modules/visualize.py
+++ modules/visualize.py
@@ -86,7 +86,7 @@ def show_download_button(df):
     st.markdown("<div>", unsafe_allow_html=True)
     st.write("### ⬇ Download Processed Dataset")
     st.markdown(
-                '<a href="data:file/csv;base64,' + df.to_csv(index=False) + '" download="balanced_data.csv">'
+                '<a href="data:file/csv;base64,' + df.to_csv(index=False).encode('utf-8') + '" download="balanced_data.csv">'
                 '<button>Download Balanced CSV</button>'
                 '</a>',
                 unsafe_allow_html=True
